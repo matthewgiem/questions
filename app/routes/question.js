9c9a5397247ce07bced4f2c679aceae5e182210a
Matthew Giem <matthew.giem@gmail.com>
2016-08-21 17:10:22 -0700

    try and get the opening of the edit form to work

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,7 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  editQuestionForm: false,
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
+  actions:  {
+    updateQuesionComponent()  {
+      this.set('editQuestionForm', false);
+      console.log("why the fuck isn't this working");
+    },
+    editQuestionFormButton() {
+      this.set('editQuestionForm', true);
+      console.log("why the fuck isn't this working FFUUCCKK");
+      console.log('try and put this in a component');
+    }
+  }
 });
