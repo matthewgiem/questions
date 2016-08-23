6a50a7075f3ac574e39616215784a0757463b2ef
Matthew Giem <matthew.giem@gmail.com>
2016-08-22 18:06:59 -0700

    change your page to the index when you add a new question

--- app/routes/questionadd.js
+++ app/routes/questionadd.js
@@ -3,9 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions:  {
     submitQuestionToServer(params)  {
-      console.log(params);
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
+      this.transitionTo('index');
     }
   }
 });
